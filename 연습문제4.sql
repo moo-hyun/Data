962025caff5b3fc6e4971185482b65fe73f2da26
vkfksvoeld <vkfksvoeld@gmail.com>
2023-06-30 18:05:50 +0900

    06/30 수업자료

--- 연습문제4.sql
+++ 연습문제4.sql
@@ -47,6 +47,16 @@ INSERT INTO `Book` VALUES (8,'야구를 부탁해','이상미디어',13000);
 INSERT INTO `Book` VALUES (9,'올림픽 이야기','삼성당',7500);
 INSERT INTO `Book` VALUES (10,'Olympic Champions','Pearson',13000);
 
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (1, 1,6000,'2014-07-01');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (1, 3,21000,'2014-07-03');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (2, 5,8000,'2014-07-03');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (3, 6,6000,'2014-07-04');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (4, 7,20000,'2014-07-05');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (1, 2,12000,'2014-07-07');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (4, 8,13000,'2014-07-07');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (3,10,12000,'2014-07-08');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (2,10,7000,'2014-07-09');
+INSERT INTO `Order`(`custId`,`bookId`,`salePrice`,`orderDate`) VALUES (3, 8,13000,'2014-07-10');
 
 
 #실습4-4
@@ -130,14 +140,76 @@ order BY a.`custId`;
 SELECT `name`,`saleprice` FROM `Customer` AS a
 JOIN `Order` AS b ON a.custId =b.custId order BY `name`;
 #실습4-37
-SELECT `name`, SUM(`salePrice`) FROM `Customer` AS a
-JOIN `Order` AS b ON a.custId =b.custId
-GROUP BY a.`custId`;
+SELECT
+	`name`,
+	SUM(`salePrice`)
+FROM `Customer` AS a 
+JOIN `Order` AS b  ON a.custId = b.custId
+GROUP BY a.`custId`
+order BY `name`;
+
 #실습4-38
+SELECT 
+	`name`,
+	`bookName`
+FROM `Customer` AS a 
+JOIN `Order` AS b ON a.custId = b.custId
+JOIN `Book` AS c ON b.bookId = c.bookId;
 
 #실습4-39
+SELECT 
+	`name`,
+	`bookName`
+FROM `Customer` AS a 
+JOIN `Order` AS b ON a.custId = b.custId
+JOIN `Book` AS c ON b.bookId = c.bookId
+WHERE `price` = 20000;
+
+
 #실습4-40
+SELECT 
+	`name`,
+	`salePrice`
+FROM `Customer` AS a
+LEFT JOIN `Order` AS b ON a.custId = b.custId;
+
+
 #실습4-41
+SELECT 
+	SUM(`salePrice`) AS `총매출`
+FROM `Customer` AS a
+JOIN `Order` AS b ON a.custId = b.custId
+WHERE `name`='김연아';
+
+
+SELECT 
+	SUM(`salePrice`) AS `총매출`
+FROM `Order`
+WHERE `custId`=(SELECT `custId` FROM `Customer` WHERE `name`='김연아');
+
+#실습4-42
+SELECT `bookName` FROM `Book` ORDER BY `price` DESC LIMIT 1;
+SELECT `bookName` FROM `Book` WHERE `price`=(SELECT MAX(`price`) FROM `Book`);
+
+#실습4-43
+SELECT `name` FROM `Customer` AS a 
+LEFT JOIN `Order` AS b ON a.custId = b.custId
+WHERE `orderId` IS NULL;
+
+SELECT `name` FROM `Customer` 
+	WHERE `custId` NOT IN(SELECT DISTINCT `custId` FROM `Order`);
+
+#실습4-44
+INSERT INTO `Book` SET
+								`bookId` = 11,, 
+								`bookName` = '스포츠의학',
+								`publisher` = '한솔의학서적';
+
+#실습4-45
+UPDATE `Customer` SET `address`='대한민국 부산' WHERE `custId`=5;
+
+#실습4-46
+DELETE FROM `Customer` WHERE `custId`=5;
 
 
 
